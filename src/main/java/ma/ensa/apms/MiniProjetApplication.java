f55df04b21bd9156566a8effd1196fa1f344661b
BenJbaliMehdi <mehdi7jbali@gmail.com>
2025-02-23 15:19:00 +0000

    first Commit

---
+++ src/main/java/ma/ensa/apms/MiniProjetApplication.java
@@ -0,0 +1,13 @@
+package ma.ensa.apms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjetApplication.class, args);
+    }
+
+}
